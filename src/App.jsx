825c9401664e04f5ebbc7b8b4fbfc56450c1b9fd
Dan <danu1besleaga@gmail.com>
2022-11-19 12:52:34 +0000

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,51 @@
 import "./App.css";
 
+import { useState } from "react";
+
 import Header from "./components/Header";
 import Card from "./components/Card";
 import SearchForm from "./components/SearchForm";
 
+let cards = [
+  {
+    name: "facebook/react",
+    desc: "desc1",
+    stars: 100,
+    forks: 200,
+  },
+  {
+    name: "vuejs/vue",
+    desc: "desc2",
+    stars: 100,
+    forks: 200,
+  },
+  {
+    name: "another/asdasd",
+    desc: "desc3",
+    stars: 100,
+    forks: 200,
+  },
+];
+
 function App() {
-  let cards = [
-    {
-      name: "facebook/react",
-      desc: "desc1",
-      stars: 100,
-      forks: 200,
-    },
-    {
-      name: "vuejs/vue",
-      desc: "desc2",
-      stars: 100,
-      forks: 200,
-    },
-    {
-      name: "another/asdasd",
-      desc: "desc3",
-      stars: 100,
-      forks: 200,
-    },
-  ];
+  let [filterCarts, setfilterCarts] = useState(cards);
 
   return (
     <>
       <Header />
 
       <main>
-        <SearchForm />
+        <SearchForm
+          onSerch={(value) => {
+            let result = cards.filter((val) => val.name.includes(value));
+            setfilterCarts(result);
+          }}
+        />
         <ul className="repo-cards">
-          {cards.map(function (val) {
+          {filterCarts.map(function (val, index) {
             return (
               <Card
+                key={index}
                 title={val.name}
                 desc={val.desc}
                 stars={val.stars}
--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,9 +1,21 @@
 import "./../App.css";
 
-function SearchForm() {
+import { useRef } from "react";
+
+function SearchForm(props) {
+  const inputEl = useRef(null);
+
+  //  const [myArray] = useState([]);
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    const name = inputEl.current;
+    props.onSerch(name.value);
+  };
+
   return (
-    <form className="search-form">
-      <input className="input" />
+    <form className="search-form" onClick={handleClick}>
+      <input className="input" type="text" ref={inputEl} />
       <button className="button">Searchblabla</button>
     </form>
   );
